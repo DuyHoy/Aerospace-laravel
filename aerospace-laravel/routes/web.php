c00d300ae21de03d088bbc477c579cf36bbcd226
nguyen duy <luxifetuda@gmail.com>
2019-08-10 15:37:21 +0700

    tao route toi cac views

--- aerospace-laravel/routes/web.php
+++ aerospace-laravel/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/aboutus', function () {
+    return view('template.aboutus-body');
 });
+Route::get('/whatwedo', function () {
+    return view('whatwedo.whatwedo-body');
+});
+Route::get('/capabilities', function () {
+    return view('capabilities.capabilities-body');
+});
+Route::get('/news', function () {
+    return view('news.news-body');
+});
+Route::get('/carreer', function () {
+    return view('carreer.carreer-body');
+});
+Route::get('/contactus', function () {
+    return view('contactus.contactus-body');
+});
+
