09a4a5f265bf447f132b1e3aa1d21a878c6c8596
trai <trainguyen150298@gmail.com>
2019-08-12 16:44:05 +0700

    add file carreer

---
+++ aerospace-laravel/resources/lang/en/carreer.php
@@ -0,0 +1,183 @@
+<?php
+return [
+'title1'=>'CAREERS',
+'title1-c1'=>'Join Our Team',
+
+
+'title2'=>'Current Job Openings',
+'title2-c1'=>'Swiss Machinists',
+'title2-c2'=>'Milling CNC Machinists',
+'title2-c3'=>'Inventory Controller',
+'title2-c4'=>'Quality Control Manager',
+'title2-c5'=>'Production Expeditor',
+'title2-c6'=>'View More',
+
+
+'title3'=>'Spontaneous Application',
+'title3-c1'=>'If you don’t see a role that fits your profile, then apply with our spontaneous application. TJ Aerospace supports a diverse workforce as an equal opportunity employer.',
+'title3-c2'=>'FIRST NAME',
+'title3-c3'=>'LAST NAME',
+'title3-c4'=>'E-MAIL*',
+'title3-c5'=>'PHONE NUMBER*',
+'title3-c6'=>'DESIRED POSITION',
+'title3-c7'=>'SHIFT PREFERENCE',
+'title3-c8'=>'PART-TIME OR FULL-TIME?',
+'title3-c9'=>'MESSAGE',
+'title3-c10'=>'ATTACH A RESUME (PDF OR DOC)',
+'title3-c11'=>'Drag & Drop Files Here',
+'title3-c12'=>'I Agree to Privacy Policy',
+'title3-c13'=>'SEND MESSAGE',
+
+'title4'=>'Job Opportunity',
+'title4-c1'=>'Apply For This Job',
+'title4-c2'=>'SHARE THIS POST',
+
+// title5
+
+
+'title5'=>'TJ Aerospace is a growing Aerospace manufacturing company specializing in milling/fabricating small to large high precision-machined and tight tolerance parts in Garden Grove, California.',
+
+'title5-c2'=>'Job Summary',
+
+'title5-c3'=>'The Swiss Machinist is responsible for the daily operations of the machines in their area (Milling, Turning, Mill/Turn, Swiss and EDM) and the quality of the end product.  20% Differential.',
+
+'title5-c4'=>'Essential Duties and Responsibilities:',
+
+'title5-c5'=>'The primary duties and responsibilities of the Swiss Machinist consist of but are not limited to:',
+
+'title5-c6'=>'Operation of machines in one machining department',
+
+'title5-c7'=>'Perform simple setups of machines in designated area',
+
+'title5-c8'=>'Perform in-process inspection of machined products (including documentation of results)',
+
+'title5-c9'=>'Complete and maintain paperwork for parts',
+
+'title5-c10'=>'Complete secondary operations i.e. deburr, polish, etc. in cycle time per print and process',
+
+'title5-c11'=>'Ability to read and interpret prints',
+
+'title5-c12'=>'Troubleshoot with the support from a Level III or IV Machinist, Supervisor or Engineer',
+
+'title5-c13'=>'Perform total productive machine maintenance',
+
+'title5-c14'=>'Maintain balance when completing machine set-up, production and maintenance duties',
+
+'title5-c15'=>'Continuous use of hands and upper extremities in fine and gross manipulation using various wrist positions and grip/pinch spans to operate equipment, push buttons, carry boxes, push carts, etc.',
+
+'title5-c16'=>'Frequent grip with tools requiring torque to tighten machine parts',
+
+'title5-c17'=>'Lifting parts/tools',
+
+'title5-c18'=>'Prolonged standing with occasional twisting (80-90% of the workday)',
+
+'title5-c19'=>'Able to lift up to 70lbs',
+
+'title5-c20'=>'Must be able to meet customer demand requirements for performing visual inspection and/or other product acceptance activities with or without accommodation',
+
+'title5-c21'=>'Exhibit a working knowledge of health and safety, responsible for safety awareness, while upholding and improving the culture of safety within the organization.  Identify opportunities for improvement and take action if appropriate, before a problem occurs.  Report potential hazards, unsafe working conditions, unsafe equipment, unsafe acts and injuries immediately',
+
+'title5-c22'=>'Other tasks as assigned',
+
+'title5-c23'=>'Qualifications:',
+
+'title5-c24'=>'Minimum Requirements: H.S. Diploma or equivalent',
+
+'title5-c25'=>'Required: Two-year certificate in machine trades',
+
+'title5-c26'=>'Preferred: 5+ years’ experience in CNC machining',
+
+'title5-c27'=>'Accountabilities:',
+
+'title5-c28'=>'Detects and reports defective materials or questionable conditions to the department manager',
+
+'title5-c29'=>'Perform to established production standards',
+
+'title5-c30'=>'Maintain work area and equipment in a clean orderly condition and follows prescribed safety procedures at all times',
+
+'title5-c31'=>'Authorities:',
+
+'title5-c32'=>'Stop the machine or process if safety, quality, or productivity standards are compromised',
+
+'title5-c33'=>'Competencies:',
+
+'title5-c34'=>'Able to read and interpret blueprints, use simple math to perform basic calculations and use hand-held measuring tools',
+'title5-c35'=>'Basic understanding of GD&amp;T',
+'title5-c36'=>'Demonstrated knowledge of SPC',
+'title5-c37'=>'Understanding of tools required to check products to guarantee part quality',
+'title5-c38'=>'Experience using manual inspection equipment','title5-c39'=>'Knowledge of CMM &amp; VCMM','title5-c1'=>'Strong mechanical aptitude',
+'title5-c40'=>'Demonstrated quality record',
+'title5-c41'=>'Excellent attention to detail',
+'title5-c42'=>'Strong written and verbal communication skills','title5-c43'=>'Proficient in various computer software applications such as MS Word, Excel and Outlook',
+'title5-c44'=>'Excellent problem-solving skills',
+'title5-c45'=>'Able to work efficiently with minimal supervision','title5-c46'=>'Able to work both independently and as a team','title5-c47'=>'Self-starter with great analytical skills',
+
+
+// title6
+'title6'=>'Job Summary',
+'title6'=>'TJ Aerospace is a growing Aerospace manufacturing company specializing in milling/fabricating small to large high precision-machined and tight tolerance parts in Garden Grove, California.',
+'title6'=>'Essential Duties and Responsibilities:',
+'title6'=>'The primary duties and responsibilities of the Inventory Controller consist of but are not limited to:',
+'title6'=>'Maintains inventory (Inventory Room, Hardware Room, Raw Material Stock) by identifying, labeling and placing materials and supplies in stock; recording location of inventory.',
+'title6'=>'Stores product by designating storage areas; adjusting storage areas according to production and shipping requirements; updating location database.',
+'title6'=>'Locates materials and supplies by pulling and verifying materials and supplies listed on production orders.',
+'title6'=>'Maintains in-process inventory at work centers by delivering and opening materials and supplies.',
+'title6'=>'Documents materials and supplies disposition by recording units delivered and location of units.',
+'title6'=>'Verifies product inventory reports by comparing logs and reports; adjusting entries.',
+'title6'=>'Prepares orders by processing requests and supply orders; pulling materials; packing boxes; placing orders in delivery area.',
+'title6'=>'Documents actions by completing forms, reports, logs and records; maintaining databases.',
+'title6'=>'Monitors inventory by completing counts as required.',
+'title6'=>'Receives credit-return material and supplies from production by verifying materials and supplies code and lot number and quantity; placing materials in stock.',
+'title6'=>'Prepares finished stock for shipment by identifying, pulling, packing, crating, loading and securing product.',
+'title6'=>'Prepares product for shipment by verifying location; directing movement to staging area; resolving problems.',
+'title6'=>'Documents product shipment by recording units shipped.',
+'title6'=>'Maintains material-handling equipment by completing pre-use inspections; making operator repairs.',
+'title6'=>'Picks-up product by monitoring production; loading and moving product with a forklift.',
+'title6'=>'Completes deliveries by driving truck or van to and from vendors.',
+'title6'=>'Maintains quality service by following organization standards.',
+'title6'=>'Maintains safe and clean work environment by keeping shelves, pallet area and workstations neat; maintaining clean shipping supply area; complying with procedures, rules and regulations.',
+'title6'=>'Contributes to team effort by accomplishing related results as needed.',
+'title6'=>'Desired Skills and Experience:',
+'title6'=>'Experience in a high volume manufacturing environment: AS9100 and First Article Inspection',
+'title6'=>'Organization; analyzing information; deal with complexity; detail-oriented; deadline-oriented; time management; process improvement; safety management; lifting; data entry skills',
+'title6'=>'Knowledge of scheduling, production workflow and planning procedures',
+'title6'=>'Ability and knowledge to operate a forklift',
+'title6'=>'Familiar with ERP programs such as JobBoss',
+'title6'=>'Working knowledge of Microsoft Office',
+'title6'=>'Intermediate math skills',
+'title6'=>'Ability to multi-task',
+'title6'=>'Experience with multiple aerospace customer specifications.',
+
+
+'title7'=>'Job Summary',
+'title7'=>'QC Manager will be responsible for performing Aerospace precision inspection verifications on mechanical parts, components, assemblies, and materials using variable or attribute measuring instruments and techniques to ensure conformance with the company’s standards.  May include but is not limited to first article parts, in-process and final inspection on assemblies, and materials for workmanship and conformance to specifications/drawings.',
+'title7'=>'Essential Duties and Responsibilities:',
+'title7'=>'The primary duties and responsibilities of the Quality Control Manager consist of but are not limited to:',
+'title7'=>'Perform Receiving Inspection, In-Process Inspection of parts being manufactured or assembled, First Article Inspection of newly designed or revised products to meet and conform with customers’ specifications',
+'title7'=>'Conferring with vendors and/or customers regarding inspection results and inspection data, and compares findings with specifications to ensure conformance to standards',
+'title7'=>'Review, release and monitor work orders/job travelers to ensure the inspector’s ability to verify requirements to meet the drawings/specifications and to check for conflicting or missing requirements on the documents',
+'title7'=>'Assures product quality through maintenance of a quality system in accordance with the quality regulations and customer requirements',
+'title7'=>'Ensuring all non-conformance product is correctly identified and isolated until disposition (rework or scrap) can be determined',
+'title7'=>'Performing a quick visual inspection, testing, measuring, analyzing products and recording pass/fail grade of products for management review',
+'title7'=>'Regular, consistent and punctual attendance is required; must be able to work overtime and weekend if necessary',
+'title7'=>'Desired Skills and Experience:',
+'title7'=>'MUST have experience in Aerospace CNC manufacturing environment, AS9100 and First Article Inspection',
+'title7'=>'CMM and Portable Arm experience is preferred, with sufficient PC-DMIS and Verisurf skills',
+'title7'=>'Blueprint reading, engineering drawings and process specifications interpretation skills are strongly preferred',
+'title7'=>'Ability to use precision measuring instruments and calibrations',
+'title7'=>'Working knowledge of Quality Assurance, metrology principles and well-versed in NADCAP, ISO and AS9100',
+'title7'=>'Knowledge of the materials, products, and the commodity assigned areas of specialization',
+'title7'=>'Working knowledge utilizing ERP system, preferably JobBOSS and usage of Microsoft Office Suite',
+'title7'=>'Experience Required:             5+ Years',
+'title7'=>'Career Level Required:          Experienced Manager',
+'title7'=>'Job Time/Status:                    Employee / Full-Time',
+ 
+
+
+
+ 
+
+
+
+
+];
---
+++ aerospace-laravel/resources/lang/vi/carreer.php
@@ -0,0 +1,185 @@
+<?php
+return [
+'title1'=>'NGHỀ NGHIỆP',
+'title1-c1'=>'Tham gia đội ngũ của chúng tôi',
+
+
+'title2'=>'Công việc hiện tại',
+'title2-c1'=>'Thụy Sĩ Machinists',
+'title2-c2'=>'Phay CNC Machinists',
+'title2-c3'=>'Điều khiển hàng tồn kho',
+'title2-c4'=>'Quản lý kiểm soát chất lượng',
+'title2-c5'=>'Dẩy nhanh sản xuất',
+'title2-c6'=>'Xem thêm',
+
+
+'title3'=>'Ứng dụng tự phát',
+'title3-c1'=>'Nếu bạn không thấy vai trò phù hợp với tiểu sử của mình, hãy áp dụng với ứng dụng tự phát của chúng tôi. TJ Aerospace hỗ trợ một lực lượng lao động đa dạng như một nhà tuyển dụng cơ hội bình đẳng.',
+'title3-c2'=>'TÊN ĐẦU TIÊN',
+'title3-c3'=>'TÊN CUỐI',
+'title3-c4'=>'E-MAIL',
+'title3-c5'=>'SỐ ĐIỆN THOẠI *',
+'title3-c6'=>'VỊ TRÍ MONG MUỐN',
+'title3-c7'=>'THAY ĐỔI TÙY CHỌN',
+'title3-c8'=>'BÁN THỜI GIAN HOẶC TOÀN THỜI GIAN?',
+'title3-c9'=>'THƯ',
+'title3-c10'=>'ĐÍNH KÈM MỘT RESUME (PDF HOẶC DOC)',
+'title3-c11'=>'Drag & Drop Files Here',
+'title3-c12'=>'Tôi đồng ý với chính sách bảo mật',
+'title3-c13'=>'GỬI TIN NHẮN',
+
+'title4'=>'Cơ hội việc làm',
+'title4-c1'=>'Ứng tuyển cho công việc này',
+'title4-c2'=>'CHIA SẺ BÀI ĐĂNG NÀY',
+
+// title5
+
+
+'title5'=>'TJ Aerospace là một công ty sản xuất hàng không vũ trụ đang phát triển chuyên về phay/chế tạo các bộ phận khoan dung nhỏ và có độ chính xác cao ở Garden Grove, California.',
+
+'title5-c2'=>'Tóm tắt công việc',
+
+'title5-c3'=>'Thợ Machon Thụy sĩ chịu trách nhiệm cho các hoạt động hàng ngày của các máy trong khu vực của họ (phay, Turning, Mill/Turn, Swiss và EDM) và chất lượng của sản phẩm cuối cùng.  20 Differential.',
+
+'title5-c4'=>'Nhiệm vụ và trách nhiệm thiết yếu:',
+
+'title5-c5'=>'Các nhiệm vụ chính và trách nhiệm của các thợ làm người Thụy sĩ bao gồm nhưng không giới hạn:',
+
+'title5-c6'=>'Hoạt động của máy trong một bộ phận gia công',
+
+'title5-c7'=>'Thực hiện các thiết lập đơn giản của máy trong khu vực chỉ định',
+
+'title5-c8'=>'Thực hiện kiểm tra trong quá trình sản phẩm gia công (bao gồm cả tài liệu về kết quả)',
+
+'title5-c9'=>'Hoàn thành và duy trì giấy tờ cho các bộ phận',
+
+'title5-c10'=>'Hoàn thành các hoạt động phụ tức là deburr, Polish, vv trong chu kỳ thời gian cho mỗi in và quy trình',
+
+'title5-c11'=>'Khả năng đọc và giải thích các bản in',
+
+'title5-c12'=>'Khắc phục sự cố với sự hỗ trợ của một cấp III hoặc IV thợ máy, giám sát viên hoặc kỹ sư',
+
+'title5-c13'=>'Thực hiện bảo trì máy sản xuất tổng số',
+
+'title5-c14'=>'Duy trì sự cân bằng khi hoàn tất thiết lập máy, nhiệm vụ sản xuất và bảo trì',
+
+'title5-c15'=>'Sử dụng liên tục tay và chi trên trong thao tác tốt và tổng sử dụng các vị trí cổ tay khác nhau và kẹp/pinch kéo dài để vận hành thiết bị, nút bấm, hộp đựng, đẩy xe, vv.',
+
+'title5-c16'=>'Báng cầm thường xuyên với các công cụ đòi hỏi mô-men xoắn để siết chặt các bộ phận máy',
+
+'title5-c17'=>'Phụ tùng/công cụ nâng',
+
+'title5-c18'=>'Đứng kéo dài với xoắn thường xuyên (80-90% của ngày làm việc)',
+
+'title5-c19'=>'Có thể nâng lên đến 70lb',
+
+'title5-c20'=>'Phải có khả thi đáp ứng nhu cầu của khách hàng để thực hiện kiểm tra trực quan và/hoặc các hoạt động chấp nhận sản phẩm khác có hoặc không có chỗ ở',
+
+'title5-c21'=>'Triển lãm kiến thức làm việc về sức khỏe và an toàn, chịu trách nhiệm về nhận thức về an toàn, đồng thời duy trì và cải thiện văn hóa an toàn trong tổ chức.  Xác định cơ hội để cải thiện và hành động nếu thích hợp, trước khi xảy ra sự cố.  Báo cáo nguy cơ tiềm ẩn, điều kiện làm việc không an toàn, thiết bị không an toàn, hành vi không an toàn và thương tích',
+
+'title5-c22'=>'Các tác vụ khác được gán',
+
+'title5-c23'=>'Bằng cấp',
+
+'title5-c24'=>'Yêu cầu tối thiểu: HS Diploma hoặc tương đương',
+
+'title5-c25'=>'Yêu cầu: chứng chỉ hai năm trong ngành nghề máy',
+
+'title5-c26'=>'Ưu tiên: 5 + năm kinh nghiệm trong gia công CNC',
+
+'title5-c27'=>'Nằm',
+
+'title5-c28'=>'Phát hiện và báo cáo các tài liệu bị lỗi hoặc điều kiện đáng ngờ cho người quản lý bộ phận',
+
+'title5-c29'=>'Thực hiện thành lập các tiêu chuẩn sản xuất',
+
+'title5-c30'=>'Duy trì khu vực làm việc và thiết bị trong sạch có trật tự và sau các thủ tục an toàn theo quy định ở mọi thời',
+
+'title5-c31'=>'Cơ quan:',
+
+'title5-c32'=>'Dừng máy hoặc quá trình nếu an toàn, chất lượng, hoặc tiêu chuẩn năng suất bị tổn hại',
+
+'title5-c33'=>'Năng lực',
+
+'title5-c34'=>'Có thể đọc và giải thích bản vẽ, sử dụng toán học đơn giản để thực hiện các phép tính và sử dụng các công cụ đo cầm tay',
+'title5-c35'=>'Sự hiểu biết cơ bản về GD&T',
+'title5-c36'=>'Chứng minh kiến thức của SPC',
+'title5-c37'=>'Hiểu các công cụ cần thiết để kiểm tra sản phẩm để đảm bảo chất lượng một phần',
+'title5-c38'=>'Kinh nghiệm sử dụng thiết bị kiểm tra thủ công','title5-c39'=>'Kiến thức về CMM & VCMM',
+'title5-c1'=>'Aptitude cơ học mạnh mẽ',
+'title5-c40'=>'Chứng minh chất lượng hồ sơ',
+'title5-c41'=>'Rất chú ý đến từng chi tiết',
+'title5-c42'=>'Kỹ năng giao tiếp bằng văn và bằng lời nói','title5-c43'=>'Thành thạo trong các ứng dụng phần mềm máy tính khác nhau như MS Word, Excel và Outlook',
+'title5-c44'=>'Kỹ năng giải quyết vấn đề tuyệt vời',
+'title5-c45'=>'Có thể làm việc hiệu quả với sự giám sát tối thiểu','title5-c46'=>'Có thể làm việc độc lập và một nhóm',
+'title5-c47'=>'Tự khởi động với kỹ năng phân tích tuyệt vời',
+
+
+// title6
+'title6'=>'Tóm tắt công việc',
+'title6-c1'=>'TJ Aerospace là một công ty sản xuất hàng không vũ trụ đang phát triển chuyên về phay/chế tạo nhỏ đến độ chính xác cao và gia công lớn và phần khoan dung chặt chẽ ở Garden Grove, California.',
+'title6-c2'=>'Nhiệm vụ và trách nhiệm thiết yếu:',
+'title6-c3'=>'Các nhiệm vụ chính và trách nhiệm của tiến trình sản xuất bao gồm nhưng không giới hạn:',
+'title6-c4'=>'Giao tiếp hiệu quả như liên lạc giữa các hoạt động, thành viên nhóm, khách hàng và các nhà cung cấp trên bất kỳ vấn đề sản xuất và chuỗi cung ứng liên quan đến lịch trình sản xuất.',
+'title6-c5'=>'Tương tác với nhà cung cấp và các bộ xử lý bên ngoài hàng ngày để đảm bảo đơn đặt hàng được giao đúng thời gian và trong thông số kỹ thuật.',
+'title6-c6'=>'Giao tiếp với nhà cung cấp và bộ vi xử lý bên ngoài để giải quyết các vấn đề có thể phát sinh.',
+'title6-c7'=>'Đảm bảo các bộ phận chất lượng được giao cho khách hàng đúng thời gian.',
+'title6-c8'=>'Tiến hành các hoạt động chậm trễ lịch trình và thay đổi lịch biểu để đáp các điều kiện không lường được bằng việc giao tiếp với các phòng ban, nhà cung cấp và khách hàng khác nhau.',
+'title6-c9'=>'Lập kế hoạch luồng công việc cho từng bộ phận và hoạt động theo trình tự sản xuất và lần dẫn đã thiết lập.',
+'title6-c10'=>'Lập kế hoạch trình tự chế tạo, lắp ráp, lắp đặt và hoạt động sản xuất khác cho sự hướng dẫn của công nhân sản xuất.',
+'title6-c11'=>'Cải thiện hiệu suất giao hàng theo thời gian bằng việc tương tác với tất cả các lĩnh vực hoạt động của thực vật.',
+'title6-c12'=>'Giao tiếp hiệu quả như liên lạc giữa các hoạt động, thành viên nhóm, khách hàng và các nhà cung cấp trên bất kỳ vấn đề sản xuất và chuỗi cung ứng liên quan đến lịch trình sản xuất.',
+'title6-c13'=>'Tương tác với nhà cung cấp và các bộ xử lý bên ngoài hàng ngày để đảm bảo đơn đặt hàng được giao đúng thời gian và trong thông số kỹ thuật.',
+'title6-c14'=>'Giao tiếp với nhà cung cấp và bộ vi xử lý bên ngoài để giải quyết các vấn đề có thể phát sinh.',
+'title6-c15'=>'Đảm bảo các bộ phận chất lượng được giao cho khách hàng đúng thời gian.',
+'title6-c16'=>'Tiến hành các hoạt động chậm trễ lịch trình và thay đổi lịch biểu để đáp các điều kiện không lường được bằng việc giao tiếp với các phòng ban, nhà cung cấp và khách hàng khác nhau.',
+'title6-c17'=>'Lập kế hoạch luồng công việc cho từng bộ phận và hoạt động theo trình tự sản xuất và lần dẫn đã thiết lập.',
+'title6-c18'=>'Lập kế hoạch trình tự chế tạo, lắp ráp, lắp đặt và hoạt động sản xuất khác cho sự hướng dẫn của công nhân sản xuất.',
+'title6-c19'=>'Cải thiện hiệu suất giao hàng theo thời gian bằng việc tương tác với tất cả các lĩnh vực hoạt động của thực vật.',
+'title6-c20'=>'Trao với giám sát bộ phận để xác định tình trạng của các dự án được giao.',
+'title6-c21'=>'Chuẩn bị danh sách các tài liệu, công cụ và thiết bị cần thiết.',
+'title6-c22'=>'Duy trì khả năng hiển thị hàng tuần trên bất kỳ thiếu phần nào.',
+'title6-c23'=>'Kỹ năng và kinh nghiệm mong muốn:',
+'title6-c24'=>'Kinh nghiệm trong một cao môi trường sản xuất khối lượng: AS9100 và kiểm tra điều đầu tiên',
+'title6-c25'=>'Tổ chức Phân tích thông tin đối phó với phức tạp; định hướng chi tiết; theo định hướng hạn chót; quản lý thời gian; cải tiến quy trình; quản lý an toàn; nâng kỹ năng nhập dữ liệu',
+'title6-c26'=>'Kiến thức về lập kế hoạch, quy trình quy trình sản xuất',
+'title6-c27'=>'Khả năng và kiến thức để vận hành xe nâng',
+'title6-c28'=>'Quen thuộc với ERP Các chương trình như JobBoss',
+'title6-c29'=>'Kiến thức làm việc của Microsoft Office',
+'title6-c30'=>'Toán trung cấp Kỹ năng',
+'title6-c31'=>'Khả năng đa nhiệm vụ',
+'title6-c32'=>'Kinh nghiệm với nhiều thông số kỹ thuật hàng không vũ trụ.',
+
+// title7
+'title7'=>'Tóm tắt công việc',
+'title7-c1'=>'QC Manager sẽ chịu trách nhiệm thực hiện kiểm tra chính xác hàng không vũ trụ trên các bộ phận cơ khí, linh kiện, Hệ thống và vật liệu sử dụng các dụng cụ đo lường biến thể hoặc thuộc tính và kỹ thuật để đảm bảo sự phù hợp với công ty Tiêu chuẩn.  Có thể bao gồm nhưng không giới hạn ở phần đầu tiên bài viết, trong quá trình và kiểm tra cuối cùng về hội đồng, và các vật liệu cho tay nghề và sự phù hợp với thông số kỹ thuật/bản vẽ.',
+'title7-c2'=>'Nhiệm vụ và trách nhiệm thiết yếu:',
+'title7-c3'=>'Các nhiệm vụ chính và trách nhiệm của quản lý kiểm soát chất lượng bao gồm nhưng không giới hạn:',
+'title7-c4'=>'Thực hiện kiểm tra nhận, kiểm tra trong quá trình các bộ phận đang được sản xuất hoặc lắp ráp, kiểm tra điều đầu tiên của sản phẩm mới được thiết kế hoặc sửa đổi để đáp và phù hợp với thông số kỹ thuật của khách hàng',
+'title7-c5'=>'Trao với các nhà cung cấp và/hoặc khách hàng về kết quả kiểm tra và dữ liệu kiểm tra, và so sánh các phát hiện với thông số kỹ thuật để đảm bảo phù hợp với tiêu chuẩn',
+'title7-c6'=>'Xem xét, phát hành và giám sát các lệnh làm việc/du khách công việc để đảm bảo khả năng của thanh tra để xác minh các yêu cầu để đáp ứng các bản vẽ/thông số kỹ thuật và kiểm tra các yêu cầu xung đột hoặc mất tích trên tài liệu',
+'title7-c7'=>'Đảm bảo chất lượng sản phẩm thông qua việc duy trì một hệ thống chất lượng phù hợp với các quy định chất lượng và yêu cầu của khách hàng',
+'title7-c8'=>'Đảm bảo tất cả các sản phẩm không phù hợp được xác định chính xác và cô lập cho đến khi bố trí (làm lại hoặc phế liệu) có thể được xác định',
+'title7-c9'=>'Thực hiện kiểm tra trực quan nhanh, kiểm tra, đo lường, phân tích sản phẩm và ghi âm thông qua/không cấp các sản phẩm để đánh giá quản lý',
+'title7-c10'=>'Cần có sự tham dự thường xuyên, nhất quán và đúng lúc; phải có khả thi làm thêm giờ và cuối tuần nếu cần thiết',
+'title7-c11'=>'Kỹ năng và kinh nghiệm mong muốn:',
+'title7-c12'=>'PHẢI có kinh nghiệm trong hàng không vũ trụ CNC môi trường sản xuất, AS9100 và kiểm tra điều đầu tiên',
+'title7-c13'=>'CMM và trải nghiệm Portable ARM được ưu tiên, với đầy đủ các kỹ năng PC-DMIS và Verisurf',
+'title7-c14'=>'Đọc Blueprint, bản vẽ kỹ thuật và kỹ năng giải thích của quá trình được ưu tiên mạnh mẽ',
+'title7-c15'=>'Khả năng sử dụng công cụ đo chính xác và hiệu chuẩn',
+'title7-c16'=>'Kiến thức làm việc về đảm bảo chất lượng, nguyên tắc đo lường và cũng-versed trong NADCAP, ISO và AS9100',
+'title7-c17'=>'Kiến thức về các tài liệu, sản phẩm, và lĩnh vực chuyên môn được giao hàng hóa',
+'title7-c18'=>'Kiến thức làm việc sử dụng hệ thống ERP, tốt JobBOSS và sử dụng Microsoft Office Suite',
+'title7-c19'=>'Yêu cầu kinh nghiệm: 5 + years',
+'title7-c20'=>'Cấp độ yêu cầu nghề nghiệp: quản lý có kinh nghiệm',
+'title7-c21'=>'Thời gian làm việc/tình trạng: nhân viên/toàn thời gian',
+ 
+
+
+
+ 
+
+
+
+
+];
