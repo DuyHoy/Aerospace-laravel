3d3429f72ac6787904336943638043220747130a
trai <trainguyen150298@gmail.com>
2019-08-13 00:54:45 +0700

    trans aboutus vs whatwedo 1h

--- aerospace-laravel/resources/views/whatwedo/whatwedo-article.blade.php
+++ aerospace-laravel/resources/views/whatwedo/whatwedo-article.blade.php
@@ -5,8 +5,8 @@
              <div class="container">
                 <div class="row">
                    <div class="col-md-6">
-                      <h1 class="page-title">What We Do</h1>
-                      <div class="lead">Lead with evolving capabilities in precision machining manufacturing, value-added services, integrated systems, and efficient processes.</div>
+                      <h1 class="page-title">{{trans('whatwedo.title1')}}</h1>
+                      <div class="lead">{{trans('whatwedo.title1-c1')}}</div>
                    </div>
                 </div>
              </div>
@@ -20,9 +20,9 @@
                    <p></p>
                 </div>
                 <div class="wp-block-magicblock-magicblock col-md-7">
-                   <h5 class="text-primary text-uppercase">WHY TJ AEROSPACE?</h5>
-                   <p>With us, you get collaboration, innovation, and expertise.</p>
-                   <p>As a family-owned business, we pride ourselves on our service, integrity and ingenuity.  For the last 15 years, our positive culture and high-performance standards have been the driving force behind our achievements and high level of customer satisfaction.</p>
+                   <h5 class="text-primary text-uppercase">{{trans('whatwedo.title2-c1')}}</h5>
+                   <p>{{trans('whatwedo.title2-c2')}}</p>
+                   <p>{{trans('whatwedo.title2-c3')}}</p>
                 </div>
                 <p></p>
              </div>
@@ -97,44 +97,44 @@
           <div class="wp-block-media-text alignwide has-media-on-the-right bg-lightgray">
              <figure class="wp-block-media-text__media"><img src="http://new.tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-core-capabilities-1200x675.jpg" alt="Tj Aerospace Core Capabilities" class="wp-image-440" srcset="https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-core-capabilities-1200x675.jpg 1200w, https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-core-capabilities-600x338.jpg 600w, https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-core-capabilities-768x432.jpg 768w" sizes="(max-width: 1200px) 100vw, 1200px" /></figure>
              <div class="wp-block-media-text__content">
-                <h5 class="text-uppercase text-primary font-weight-semibold">Benefits</h5>
-                <h2 class="mb-4">Core Capabilities</h2>
+             <h5 class="text-uppercase text-primary font-weight-semibold">{{trans('whatwedo.title4')}}</h5>
+                <h2 class="mb-4">{{trans('whatwedo.title4-c1')}}</h2>
                 <ul class="list">
-                   <li>CNC Milling &amp; Turning of Various Materials</li>
-                   <li>Prototype to High Volume Manufacturing</li>
-                   <li>Lean, Automated Manufacturing</li>
-                   <li>Lights-Out Manufacturing</li>
-                   <li>Smart Manufacturing</li>
-                   <li>In-House Fixturing &amp; Tooling</li>
-                   <li>Additive Manufacturing</li>
-                   <li>Long-Term Contract Manufacturing</li>
-                   <li>Sheet Metal Fabrication</li>
-                   <li>Assembly/Sub-Assembly/Kitting</li>
-                   <li>Engineering</li>
-                   <li>CAM Programming Services</li>
-                   <li>Ultrasonic Multi-Stage Cleaning</li>
-                   <li>Clean Room Assembly</li>
+                   <li>{{trans('whatwedo.title4-c2')}}</li>
+                   <li>{{trans('whatwedo.title4-c3')}}</li>
+                   <li>{{trans('whatwedo.title4-c4')}}</li>
+                   <li>{{trans('whatwedo.title4-c5')}}</li>
+                   <li>{{trans('whatwedo.title4-c6')}}</li>
+                   <li>{{trans('whatwedo.title4-c7')}}</li>
+                   <li>{{trans('whatwedo.title4-c8')}}</li>
+                   <li>{{trans('whatwedo.title4-c9')}}</li>
+                   <li>{{trans('whatwedo.title4-c10')}}</li>
+                   <li>{{trans('whatwedo.title4-c11')}}</li>
+                   <li>{{trans('whatwedo.title4-c12')}}</li>
+                   <li>{{trans('whatwedo.title4-c13')}}</li>
+                   <li>{{trans('whatwedo.title4-c14')}}</li>
+                   <li>{{trans('whatwedo.title4-c15')}}</li>
                 </ul>
              </div>
           </div>
           <div class="wp-block-media-text alignwide bg-darkblue">
              <figure class="wp-block-media-text__media"><img src="http://new.tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-quality-assurance-1200x800.jpg" alt="Tj Aerospace Quality Assurance" class="wp-image-441" srcset="https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-quality-assurance-1200x800.jpg 1200w, https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-quality-assurance-600x400.jpg 600w, https://tjaerospace.com/wp-content/uploads/2019/07/tj-aerospace-quality-assurance-768x512.jpg 768w" sizes="(max-width: 1200px) 100vw, 1200px" /></figure>
              <div class="wp-block-media-text__content">
-                <h5 class="text-uppercase text-white font-weight-semibold">Quality Assurance</h5>
-                <h2 class="text-white">To Achieve Customer Satisfaction By Meeting Customer Requirements</h2>
-                <p class="text-white">Our Quality Management System will ensure effective production flow and proper quality control process to continuously improve our production yield.  Our clean, effective manufacturing operation enables us to pass on the savings to our customers.</p>
+                <h5 class="text-uppercase text-white font-weight-semibold">{{trans('whatwedo.title5')}}</h5>
+                <h2 class="text-white">{{trans('whatwedo.title5-c1')}}</h2>
+                <p class="text-white">{{trans('whatwedo.title5-c2')}}</p>
              </div>
           </div>
           <div class="wp-block-media-text alignwide has-media-on-the-right bg-primary">
              <figure class="wp-block-media-text__media"><img src="http://new.tjaerospace.com/wp-content/uploads/2019/06/proudly-serving-our-customer-since-2007-1200x800.jpg" alt="Proudly Serving Our Customer Since 2007" class="wp-image-145" srcset="https://tjaerospace.com/wp-content/uploads/2019/06/proudly-serving-our-customer-since-2007-1200x800.jpg 1200w, https://tjaerospace.com/wp-content/uploads/2019/06/proudly-serving-our-customer-since-2007-600x400.jpg 600w, https://tjaerospace.com/wp-content/uploads/2019/06/proudly-serving-our-customer-since-2007-768x512.jpg 768w" sizes="(max-width: 1200px) 100vw, 1200px" /></figure>
              <div class="wp-block-media-text__content">
-                <h5 class="text-uppercase text-white font-weight-semibold">Industries We Serve</h5>
-                <h2 class="text-white">TJ Aerospace Proudly Serves the Aerospace, Commercial, Defense and Medical Sectors.</h2>
-                <p class="text-white">TJ Aerospace is a smart and lean manufacturing shop that specializes in the production of complex, high precision, tight tolerance machined parts and sub-assembly.</p>
+                <h5 class="text-uppercase text-white font-weight-semibold">{{trans('whatwedo.title6')}}</h5>
+                <h2 class="text-white">{{trans('whatwedo.title6-c1')}}</h2>
+                <p class="text-white">{{trans('whatwedo.title6-c2')}}</p>
              </div>
           </div>
           <div id="carouselblock-block_5d1cb52306223" class="carouselblock ">
-             <h6 class="mb-3 mb-sm-4 mx-auto text-center px-3 heading">Some of Our Customers Include:</h6>
+             <h6 class="mb-3 mb-sm-4 mx-auto text-center px-3 heading">{{trans('whatwedo.title7')}}</h6>
              <div class="container">
                 <div class="owl-carousel owl-theme carouselblock-carousel">
                    <div class="item">
