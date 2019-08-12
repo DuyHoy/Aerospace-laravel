de6ed8b8bb39ffcc10640ab5f2fefeed35e4d58e
trai <trainguyen150298@gmail.com>
2019-08-12 21:22:32 +0700

    9h30 toi thu 2

--- aerospace-laravel/resources/views/carreer/carreer-show-article.blade.php
+++ aerospace-laravel/resources/views/carreer/carreer-show-article.blade.php
@@ -1,18 +1,4 @@
-<!-- @extends('carreer.carreer-layout')
-@section('carreer-body')
-<body class="job-template-default single single-job postid-865 cookies-set cookies-accepted woocommerce-no-js swiss-machinists">
-@include('static.navbar')
-    
-    @include('static.header')
-     
-    @include('carreer.carreer-article')   
-    
-    
-    @include('static.footer')
-    
-    @include('static.script')
-</body>    
-@endsection -->
+
 <article class="post-865 job type-job status-publish hentry"  itemscope itemtype="http://schema.org/Article">
 
     <main role="main" class="main">
